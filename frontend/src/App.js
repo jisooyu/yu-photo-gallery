5e0a730bf5d39a884ff07dbcd4976053a7ba2826
jisooyu <jisooyu@hotmail.com>
2022-05-24 18:23:12 +0900

    applying tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
 import NewGallery from './pages/NewGallery';
 
 function App() {
   return (
-    <div className='App'>
+    <>
       <NewGallery />
-    </div>
+    </>
   );
 }
 
--- frontend/src/pages/NewGallery.js
+++ frontend/src/pages/NewGallery.js
@@ -24,20 +24,18 @@ const NewGallery = () => {
   };
 
   return (
-    <div className='container bg-gray-200 rounded-xl shade'>
-      <h1 className='text-2xl mx-auto text-gray-800 front-bold bg-gray-200'>
+    <>
+      <h1 className='bg-indigo-500 text-center p-4 text-4xl text-white'>
         AWS S3에서 사진 불러오기
       </h1>
-      <form
-        onSubmit={handleSubmit}
-        className='text-xl text-gray-800 front-bold mb-5'>
+      <form onSubmit={handleSubmit} className='flex justify-center mt-8'>
         <label
-          htmlFor='picturename'
-          className='text-xl text-gray-700 front-bold mb-5'>
+          htmlFor='formFile'
+          className='form-label inline-block mr-4 font-extrabold text-2xl text-gray-700'>
           Picture Name:
         </label>
         <input
-          className='bg-gray-200 mb-3'
+          className=' text-2xl border-2 bg-indigo-200 mr-5'
           name='picturename'
           type='text'
           id='picturename'
@@ -45,15 +43,32 @@ const NewGallery = () => {
           onChange={(e) => setPicturename(e.target.value)}
         />
         <input
+          className='form-control
+            block
+            w-50
+            mr-5
+            px-5
+            py-1.5
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
           type='file'
           name='imageFile'
           accept='image/jpeg, image/jp, image/png'
           onChange={handleFileChange}
         />
-        <button>Submit</button>
+        <button className='w-25 p-2 border-2 bg-indigo-500 border-indigo-400 border-solid rounded-2xl  text-2xl text-white font-bold'>
+          Submit
+        </button>
       </form>
-      <img src={s3Location} alt={'images-from-s3'} className='image' />
-    </div>
+      <img src={s3Location} alt='' className='text-center mt-10 mx-auto' />
+    </>
   );
 };
 
